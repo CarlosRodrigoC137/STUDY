84b693d380b2aea14405bb1579692ad6da5dbfc0
CarlosRodrigoC137 <carlosrl.contato@gmail.com>
2023-09-03 01:28:09 -0300

    finalizando aula 4 sobre Collections com método each para arrays e Hash, Map e Select

--- Ruby/My_Ruby_Studies/collections.rb
+++ Ruby/My_Ruby_Studies/collections.rb
@@ -33,6 +33,9 @@ capitais = Hash.new
 #ou
 capitais = {}
 
+#Pode-se usar o "Hash Rocket" :a => b
+#ou o formato simplificado JSON a: b para definir um Hash
+
 capitais = { sao_paulo: "São Paulo", pernambuco: "Recife", bahia: "Salvador"}
 #ou
 capitais[:rio_de_janeiro] = "Rio de Janeiro"
@@ -47,7 +50,7 @@ puts capitais.size, capitais.empty?
 
 system "clear"
 
-#Testando Método EACH
+#Testando Método EACH com Array
 #Each não altera os valores fora da função de trabalho dele próprio.
 
 randowNames = ["Alexa", "Beatriz", "Claussius"]
@@ -61,6 +64,77 @@ end
 
 puts name
 
+system "clear"
 
+#Testando Método EACH com Hash
 
+persons = { :person1 => "Alexa", :person2 => "Beatriz", :person3 => "Carlos" }
+# persons = {person1: "Alexa" ...} = json simplificado
 
+persons.each do |key, value|
+    print "#{key}, é: #{value}\n"
+end
+
+system "clear"
+
+#Testando Map:
+#O Map é usado para criar um novo array, ou atualiza o mesmo
+
+arr = [1, 2, 3, 4]
+
+new_arr = arr.map do |item|
+    item * 2
+end
+
+print "#{arr} \n #{new_arr} \n \n"
+
+#Sobrescrevedo arr:
+arr = arr.map {|i| i*3}
+print "#{arr}"
+
+system "clear"
+
+#testando método Select:
+#Select sempre retorna o que for True dentro da especificação.
+
+arr = [1, 2, 3, 4, 5, 6]
+arrSelected = arr.select { |item| item>= 3}
+print "#{arrSelected}\n"
+
+#com hash
+my_hash = {produto1: 20, produto2: 80, produto3: 30, 
+    produto4: 50, produto5: 35}
+hashSelected = my_hash.select { |key, value|
+    value >=35
+}
+print "#{hashSelected}"
+
+system "clear"
+
+#treino de arrays e Hash com uns prints:
+
+arr = [1, 2, 3]
+newArr = arr.map{ |i| i**2 }
+puts newArr
+
+hash = {:first => "one", :second => "two", :third => "three"}
+hash.each{ |key, value|
+    print "Uma das chaves é #{key} e o seu valor #{value}\n"
+}
+
+numbers = {A: 10, B: 30, C: 20, D: 25, E: 15}
+maxValue = 0
+numbers.each{ |key, value|
+    if(value>= maxValue)
+        maxValue = value
+        maxKey = key
+    end
+}
+print "Chave: #{numbers.key(maxValue)}, Valor: #{maxValue}\n"
+
+#refatorando o numbers:
+
+selected = numbers.select { |key, value|
+    value == numbers.values.max
+}
+#É importante utilizar "==" pois está fazendo uma comparação
