046bc556a2cc29ec01d04406f250ee6c3a133c22
CarlosRodrigoC137 <carlosrl.contato@gmail.com>
2023-09-04 15:11:13 -0300

    testando variaveis de classe e variáveis de instância

--- Ruby/My_Ruby_Studies/variavel.rb
+++ Ruby/My_Ruby_Studies/variavel.rb
@@ -35,3 +35,47 @@ puts minhaGlobal2.reAuterandoGlobal
 puts minhaGlobal2.reAuterandoGlobal
 
 puts $global
+
+# VARIÁVEL GLOBAL
+#Elas podem ser chamadas dentro de outras classes e escopos/métodos
+#podem ser auteradas a todo momento.
+#Não se recomenda usar.
+system "clear"
+
+class User
+    @@user_count = 0
+    def add(name)
+        puts "User #{name} adicionado"
+        @@user_count += 1
+        puts @@user_count
+    end
+end
+
+first_user = User.new
+first_user.add("Alexa")
+
+second_user = User.new
+second_user.add("Beatriz")
+
+third_user = User.new
+third_user.add("Carlos")
+
+# VARIÁVEIS DE CLASSE
+# use-se '@@'
+#Não é mito comum, pode ser auterada dentro da classe
+#ou ao interagir com ela
+system "clear"
+
+class DefinindoUser
+    def addUser(name)
+        @name = name
+        puts "User adicionado"
+        hello
+    end
+    def hello
+        puts "Seja bem vindo #{@name}"
+    end
+end
+
+user = DefinindoUser.new
+user.addUser("Alexa")
